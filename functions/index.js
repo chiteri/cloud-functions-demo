b9931aaff3bac8136f65573e1716e5683ca05f50
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2021-01-01 21:22:46 -0700

    Add functions to mail a user on sign-up and sign in

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,118 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Cloud Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've successfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+.onCreate((snap, context) => {
+  // Grab the current value of what was written to Cloud Firestore.
+  const original = snap.data().original;
+
+  // Access the parameter `{documentId}` with `context.params`
+  functions.logger.log('Uppercasing', context.params.documentId, original);
+  
+  const uppercase = original.toUpperCase();
+  
+  // You must return a Promise when performing asynchronous tasks inside a Functions such as
+  // writing to Cloud Firestore.
+  // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
+  return snap.ref.set({uppercase}, {merge: true});
+});
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'Cloud Storage for Firebase quickstart';
+
+// [START sendWelcomeEmail]
+/**
+ * Sends a welcome email to new user.
+ */
+// [START onCreateTrigger]
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+// [END onCreateTrigger]
+  // [START eventAttributes]
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName; // The display name of the user.
+  // [END eventAttributes]
+
+  return sendWelcomeEmail(email, displayName);
+});
+// [END sendWelcomeEmail]
+
+// [START sendByeEmail]
+/**
+ * Send an account deleted email confirmation to users who delete their accounts.
+ */
+// [START onDeleteTrigger]
+exports.sendByeEmail = functions.auth.user().onDelete((user) => {
+// [END onDeleteTrigger]
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendGoodbyeEmail(email, displayName);
+});
+// [END sendByeEmail]
+
+// Sends a welcome email to the given user.
+async function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+  await mailTransport.sendMail(mailOptions);
+  console.log('New welcome email sent to:', email);
+  return null;
+}
+
+// Sends a goodbye email to the given user.
+async function sendGoodbyeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+  };
+
+  // The user unsubscribed to the newsletter.
+  mailOptions.subject = `Bye!`;
+  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+  await mailTransport.sendMail(mailOptions);
+  console.log('Account deletion confirmation email sent to:', email);
+  return null;
+}
